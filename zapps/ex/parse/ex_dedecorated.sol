0709878c63a0b126eb7005661b67a6c4d4541a07
almatkhan <kuanysh.almat@gmail.com>
2024-07-22 03:15:44 +0100

    still got errors

--- zapps/ex/parse/ex_dedecorated.sol
+++ zapps/ex/parse/ex_dedecorated.sol
@@ -2,90 +2,55 @@
 pragma solidity ^0.8.21;
 
 interface IERC20 {
-    function totalSupply() external view returns (uint);
-    function balanceOf(address account) external view returns (uint);
+function totalSupply() external view returns (uint);
+function balanceOf(address account) external view returns (uint);
 
-    function transfer(address recipient, uint amount) external returns (bool);
+function transfer(address recipient, uint amount) external returns (bool);
 
-    function allowance(
-        address owner,
-        address spender
-    ) external view returns (uint);
+function allowance(
+address owner,
+address spender
+) external view returns (uint);
 
-    function approve(address spender, uint amount) external returns (bool);
+function approve(address spender, uint amount) external returns (bool);
 
-    function transferFrom(
-        address sender,
-        address recipient,
-        uint amount
-    ) external returns (bool);
+function transferFrom(
+address sender,
+address recipient,
+uint amount
+) external returns (bool);
 
-    event Transfer(address indexed from, address indexed to, uint amount);
-    event Approval(address indexed owner, address indexed spender, uint amount);
+event Transfer(address indexed from, address indexed to, uint amount);
+event Approval(address indexed owner, address indexed spender, uint amount);
 }
 
 contract ZKSender {
-    IERC20 public immutable token;
-    uint public reserve;
+IERC20 public immutable token;
+uint public reserve;
 
-    mapping(address => uint) balances;
+mapping(address => uint) public balances;
 
-    constructor(address _token) {
-        token = IERC20(_token);
-    }
+constructor(address _token) {
+token = IERC20(_token);
+}
 
-    // This function hides the tokens and makes them only accessible via ZKP
-    function vault(uint _amountIn) external returns (bool) {
-        // First, transfer the funds from the user to this contract (lock funds)
-        require(_amountIn > 0, "Amount must be greater than 0");
-        require(
-            token.transferFrom(msg.sender, address(this), _amountIn),
-            "Transfer failed"
-        );
+// This function hides the tokens and makes them only accessible via ZKP
+function vault(uint _amountIn) external returns (bool) {
+// First, transfer the funds from the user to this contract (lock funds)
+require(_amountIn > 0, "Amount must be greater than 0");
+require(
+token.transferFrom(msg.sender, address(this), _amountIn),
+"Transfer failed"
+);
 
-        // Then, update the balance of the user in the ZKP way
-        balances[msg.sender] += _amountIn;
-        reserve += _amountIn;
+// Then, update the balance of the user in the ZKP way
+balances[msg.sender] += _amountIn;
+reserve += _amountIn;
 
-        // We could apply some fees here.
-        // Alternatively, they could be applied on withdrawal
+// We could apply some fees here.
+// Alternatively, they could be applied on withdrawal
+// emit something? Emit a commitment change?
 
-        // emit something? Emit a commitment change?
-
-        return true;
-    }
-
-    // This function UNhides the tokens and makes them transparently accissable
-    // For the simplicity this function supports only full withdrawals.
-    // No partial withdrawals
-    function unvault() external returns (bool) {
-        uint amount = balances[msg.sender];
-
-        require(amount > 0, "You have no funds available");
-        require(reserve >= amount, "Unsufficiant funds"); // This should never happen!
-        require(
-            token.transferFrom(address(this), msg.sender, amount),
-            "Transfer failed"
-        );
-
-        reserve -= amount;
-
-        // We could apply some fees here, if we wanted to
-
-        // Emit something? A commitment change?
-
-        return true;
-    }
-
-    // This function sends the funds within the ZKP
-    function transfer(address recipient, uint amount) external returns (bool) {
-        require(amount > 0, "Amount must be greater than 0");
-
-        balances[msg.sender] -= amount;
-        balances[recipient] += amount;
-
-        // Emit something? A commitment change?
-
-        return true;
-    }
+return true;
+}
 }
